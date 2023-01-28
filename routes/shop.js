2eedf237411443198f3b0fbda05f2146c8dc2a38
unknown <devanshshah777@gmail.com>
2023-01-28 23:24:45 +0530

    full application with cart and edit delect product through files

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,15 @@
 const path = require('path');
 const express = require('express');
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 const shopRoute = express.Router();
 
-shopRoute.get('/',productsController.getProducts);
+shopRoute.get('/',shopController.getIndex);
+shopRoute.get('/products',shopController.getProducts);
+shopRoute.get('/products/:productId',shopController.getProduct);
+shopRoute.get('/cart',shopController.getCart);
+shopRoute.post('/cart',shopController.postCart);
+shopRoute.get('/checkout',shopController.getCheckout);
+shopRoute.get('/orders',shopController.getOrders);
+shopRoute.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 module.exports = shopRoute;
