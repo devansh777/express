962b05762afe7d849ffaabc9d27595b5f319f9dc
unknown <devanshshah777@gmail.com>
2023-01-28 10:16:24 +0530

    model which save data in json file

--- controllers/products.js
+++ controllers/products.js
@@ -14,6 +14,13 @@ exports.postAddProduct = (req, res)=>{
 }
 
 exports.getProducts = (req, res, next)=>{
-    const products = Product.fetchAll();
-    res.render('shop',{prods: products, pageTitle : "Shop", path: "/"});
+    Product.fetchAll(products=>{
+        res.render(
+            'shop',{
+                prods: products,
+                pageTitle : "Shop",
+                path: "/"
+            }
+        );
+    });
 }
--- models/product.js
+++ models/product.js
@@ -1,15 +1,36 @@
-products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+    );
+
+const getProductFromFile = cb => {
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            cb([]);
+        }else{
+            cb(JSON.parse(fileContent));
+        }
+    })
+}
 
 module.exports = class Prduct{
     constructor(t){
         this.title = t;
     }
-
     save() {
-        products.push(this);
+        getProductFromFile(products => {
+            products.push(this);
+            fs.writeFile(p,JSON.stringify(products),(err)=>{
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){    //callBack
+        getProductFromFile(cb);
     }
 }
