172b77d75e8d2aabfb756c635f2411b64210a71b
unknown <devanshshah777@gmail.com>
2023-02-20 02:07:41 +0530

    fix design and add auth route and controller for cookie

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const routes = express.Router();
+const authController = require('../controllers/auth');
+
+routes.get('/login',authController.getLogin);
+routes.post('/login',authController.postLogin);
+
+module.exports = routes;
